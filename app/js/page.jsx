9b5091f8dc6b598c5f05e7392f2a7e4658d702b1
Tamal Das <tamalcodes@gmail.com>
2023-12-09 21:46:30 +0530

    feat: added javascript

--- app/js/page.jsx
+++ app/js/page.jsx
@@ -5,6 +5,7 @@ const Javascript = () => {
   return (
     <main>
       <h1 className="heading">Javascript</h1>
+
       <p className="w-[60%] mt-4 text-[20px] text-yellow-600 font-poppins">
         For the sake of simplicity i have divided the Javsacript into multiple
         sections based on the topics & personal choices. Click on the topic you
@@ -15,8 +16,45 @@ const Javascript = () => {
         <Link href={"/js/arrays"} className="card">
           <h1 className="heading text-[40px] text-yellow-600">Arrays</h1>
         </Link>
+
         <Link href={"/js/promises"} className="card">
           <h1 className="heading text-[40px] text-yellow-600">Promises</h1>
+        </Link>
+
+        <Link href={"/js/browsers"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Browsers</h1>
+        </Link>
+
+        <Link href={"/js/closures"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Closures</h1>
+        </Link>
+
+        <Link href={"/js/engine"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Engine</h1>
+        </Link>
+
+        <Link href={"/js/functions"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Functions</h1>
+        </Link>
+
+        <Link href={"/js/events"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Events</h1>
+        </Link>
+
+        <Link href={"/js/hoisting"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Let/Var</h1>
+        </Link>
+
+        <Link href={"/js/debthro"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Debounce</h1>
+        </Link>
+
+        <Link href={"/js/objects"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Objects</h1>
+        </Link>
+
+        <Link href={"/js/misc"} className="card">
+          <h1 className="heading text-[40px] text-yellow-600">Misc</h1>
         </Link>
       </div>
     </main>
